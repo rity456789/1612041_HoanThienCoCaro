5c7c41dc8e0ac61c4905d9f329e3267bfe8fb38d
binhph <binhph>
2019-09-25 21:52:03 +0700

    change

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -3,7 +3,7 @@ import '../App.css';
 
 class Square extends React.Component {
     render() {
-        const className = 'square' + (this.props.highlight ? ' highlight' : '');
+        const className = 'square' + (this.props.isHighlight ? ' highlight' : '');
         return (
             <button className={className} onClick={this.props.onClick}>
                 {this.props.value}
