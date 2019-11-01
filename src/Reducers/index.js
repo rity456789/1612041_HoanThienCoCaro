5f69a51f19a6902955b5a5282e55a72e00fff7d3
binhph <binhph>
2019-11-01 16:38:07 +0700

    use redux successfully

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -18,87 +18,17 @@ const initialState = {
   isAscending: true
 };
 
-function CoCaro(state = initialState, action) {
-  switch (action.type) {
-    case MOVES_ORDER:
-      return { ...state, isAscending: !state.isAscending };
-
-    case GO_TO_MOVE:
-      return {
-        ...state,
-        historyTable: state.historyTable.slice(0, action.step + 1),
-        stepNumber: action.step,
-        xIsNext: action.step % 2 === 0
-      };
-
-    case RESTART_GAME:
-      return {
-        ...state,
-        historyTable: [
-          {
-            squares: Array(400).fill(null)
-          }
-        ],
-        stepNumber: 0,
-        xIsNext: true,
-        winner: null,
-        winLine: null,
-        isAscending: true
-      };
-
-    case SELECT_SQUARE:
-      const i = action.i;
-      const { historyTable, stepNumber } = state;
-      const history = historyTable.slice(0, stepNumber + 1);
-      const current = history[history.length - 1];
-      const squares = current.squares.slice();
-      const { winner } = state;
-      if (winner || squares[i]) {
-        break;
-      }
-      const { xIsNext } = state;
-      squares[action.index] = xIsNext ? 'X' : 'O';
-      // state = this.calculateWinner(i);
-      return {
-        ...state,
-        historyTable: historyTable.concat([
-          {
-            squares,
-            latestMoveSquare: i
-          }
-        ]),
-        stepNumber: historyTable.length,
-        xIsNext: !xIsNext
-      };
-
-    default:
-      return state;
-  }
-}
-/*
-calculateWinner = i => {
-  // check win vertical line
-  state = this.checkWinVerticalLine(i);
-  // check win horizontal line
-  state = this.checkWinHorizontalLine(i);
-  // check win first (\) diagonal line
-  state = this.checkWinFirstDiagonalLine(i);
-  // check win second (/) diagonal line
-  state = this.checkWinSecondDiagonalLine(i);
-};
-
-checkWinVerticalLine = i => {
-  const { historyTable, stepNumber } = this.state;
-  const history = historyTable.slice(0, stepNumber + 1);
-  const current = history[history.length - 1];
-  const squares = current.squares.slice();
-  const { xIsNext } = this.state;
-  squares[i] = xIsNext ? 'X' : 'O';
+function checkWinVerticalLine(i, squares) {
   let length = 1;
   let chanDauTren = false;
   let chanDauDuoi = false;
   const line = new Array(20);
   line.push(i);
+
+  const res = {
+    winner: null,
+    winLine: null
+  };
 
   // check upper
   // co the lap het squares r check line nhung se ton chi phi
@@ -124,27 +54,23 @@ checkWinVerticalLine = i => {
   }
   // win
   if (length >= 5 && (!chanDauTren || !chanDauDuoi)) {
-    return {
-      ...state,
-      winner: squares[i],
-      winLine: line
-    };
+    res.winner = squares[i];
+    res.winLine = line;
   }
-  return state;
-};
+  return res;
+}
 
-checkWinHorizontalLine = i => {
-  const { historyTable, stepNumber } = this.state;
-  const history = historyTable.slice(0, stepNumber + 1);
-  const current = history[history.length - 1];
-  const squares = current.squares.slice();
-  const { xIsNext } = this.state;
-  squares[i] = xIsNext ? 'X' : 'O';
+function checkWinHorizontalLine(i, squares) {
   let length = 1;
   let chanDauTrai = false;
   let chanDauPhai = false;
   const line = new Array(20);
   line.push(i);
+
+  const res = {
+    winner: null,
+    winLine: null
+  };
 
   // check left
   // co the lap het squares r check line nhung se ton chi phi
@@ -170,27 +96,23 @@ checkWinHorizontalLine = i => {
   }
   // win
   if (length >= 5 && (!chanDauTrai || !chanDauPhai)) {
-    return {
-      ...state,
-      winner: squares[i],
-      winLine: line
-    };
+    res.winner = squares[i];
+    res.winLine = line;
   }
-  return state;
-};
+  return res;
+}
 
-checkWinFirstDiagonalLine = i => {
-  const { historyTable, stepNumber } = this.state;
-  const history = historyTable.slice(0, stepNumber + 1);
-  const current = history[history.length - 1];
-  const squares = current.squares.slice();
-  const { xIsNext } = this.state;
-  squares[i] = xIsNext ? 'X' : 'O';
+function checkWinFirstDiagonalLine(i, squares) {
   let length = 1;
   let chanDauTren = false;
   let chanDauDuoi = false;
   const line = new Array(20);
   line.push(i);
+
+  const res = {
+    winner: null,
+    winLine: null
+  };
 
   // check upper
   // co the lap het squares r check line nhung se ton chi phi
@@ -216,27 +138,23 @@ checkWinFirstDiagonalLine = i => {
   }
   // win
   if (length >= 5 && (!chanDauTren || !chanDauDuoi)) {
-    return {
-      ...state,
-      winner: squares[i],
-      winLine: line
-    };
+    res.winner = squares[i];
+    res.winLine = line;
   }
-  return state;
-};
+  return res;
+}
 
-checkWinSecondDiagonalLine = i => {
-  const { historyTable, stepNumber } = this.state;
-  const history = historyTable.slice(0, stepNumber + 1);
-  const current = history[history.length - 1];
-  const squares = current.squares.slice();
-  const { xIsNext } = this.state;
-  squares[i] = xIsNext ? 'X' : 'O';
+function checkWinSecondDiagonalLine(i, squares) {
   let length = 1;
   let chanDauTren = false;
   let chanDauDuoi = false;
   const line = new Array(20);
   line.push(i);
+
+  const res = {
+    winner: null,
+    winLine: null
+  };
 
   // check upper
   // co the lap het squares r check line nhung se ton chi phi
@@ -262,13 +180,92 @@ checkWinSecondDiagonalLine = i => {
   }
   // win
   if (length >= 5 && (!chanDauTren || !chanDauDuoi)) {
-    return {
-      ...state,
-      winner: squares[i],
-      winLine: line
-    };
+    res.winner = squares[i];
+    res.winLine = line;
   }
-  return state;
-};
-*/
+  return res;
+}
+
+function calculateWinner(i, squares) {
+  let res = {
+    winner: null,
+    winLine: null
+  };
+  // check win vertical line
+  res = checkWinVerticalLine(i, squares);
+  if (res.winner != null) return res;
+
+  // check win horizontal line
+  res = checkWinHorizontalLine(i, squares);
+  if (res.winner != null) return res;
+
+  // check win first (\) diagonal line
+  res = checkWinFirstDiagonalLine(i, squares);
+  if (res.winner != null) return res;
+
+  // check win second (/) diagonal line
+  res = checkWinSecondDiagonalLine(i, squares);
+  return res;
+}
+
+function CoCaro(state = initialState, action) {
+  switch (action.type) {
+    case MOVES_ORDER:
+      return { ...state, isAscending: !state.isAscending };
+
+    case GO_TO_MOVE:
+      if (state.winner) return state;
+      return {
+        ...state,
+        historyTable: state.historyTable.slice(0, action.step + 1),
+        stepNumber: action.step,
+        xIsNext: action.step % 2 === 0
+      };
+
+    case RESTART_GAME:
+      return {
+        ...state,
+        historyTable: [
+          {
+            squares: Array(400).fill(null)
+          }
+        ],
+        stepNumber: 0,
+        xIsNext: true,
+        winner: null,
+        winLine: null,
+        isAscending: true
+      };
+
+    case SELECT_SQUARE:
+      const i = action.i;
+      const { historyTable } = state;
+      const current = historyTable[historyTable.length - 1];
+      const squares = current.squares.slice();
+      const { winner } = state;
+      if (winner || squares[i]) {
+        return state;
+      }
+      const { xIsNext } = state;
+      squares[action.i] = xIsNext ? 'X' : 'O';
+      const res = calculateWinner(i, squares);
+      return {
+        ...state,
+        historyTable: historyTable.concat([
+          {
+            squares,
+            latestMoveSquare: i
+          }
+        ]),
+        stepNumber: historyTable.length,
+        xIsNext: !xIsNext,
+        winner: res.winner,
+        winLine: res.winLine
+      };
+
+    default:
+      return state;
+  }
+}
+
 export default CoCaro;
